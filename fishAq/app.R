2abd940d39acba00c5636cdea08993bdbd07dfac
ramrhodes <rachrhodes@gmail.com>
2021-02-25 10:53:53 -0800

    added text to an overview slide

--- fishAq/app.R
+++ fishAq/app.R
@@ -6,14 +6,29 @@ library(shinythemes)
 library(janitor)
 
 app_data <- read_csv("appDatCEOA.csv") %>%
-  mutate(farmsize = case_when(frmSz == 2 ~ "small",
-                              frmSz == 5 ~ "medium",
-                              frmSz == 10 ~ "large"))
+  clean_names() %>%
+  mutate(frmsz_class = case_when(frm_sz == 2 ~ "small",
+                              frm_sz == 5 ~ "medium",
+                              frm_sz == 10 ~ "large")) %>%
+  mutate(num_farms = ((mgmt_area*100)/frm_sz))
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(theme = shinytheme("cosmo"),
 
   navbarPage("Bioeconomic Model: Fish & Protected Space",
+             tabPanel("Overview",
+                      mainPanel(
+                        h3("Introduction"),
+                        p("This Shiny application uses data from Jessica Couture's research to explore the impacts of Marine Protected Areas (MPA) and farms on wild fish populations and fishery catches. The application provides a platform for the user to observe how variations in size of MPAs and aquaculture farms relate to biomass of fish catches, fish attraction to the farm, and movement of adult fish."),
+                        h3("Overview"),
+                        p("The model is a 1-D spatially explicit bioeconomic model that simulates fish movement and fishery catches around a protected space. The model is used to simulate the impacts of an MPA or farm on wild populations (abundance, biomass) and fishery catches (yield amount and yield biomass)."),
+                        img(src = "shiny_app_diagram.png"),
+                        h3("Summary"),
+                        h3("Citation"),
+                        p("Model created by Jessica Couture, University of California, Santa Barbara")
+                      )
+                      ),
+
              tabPanel("Farm Size",
                       sidebarLayout(
                         sidebarPanel("Select Farm size",
@@ -113,19 +128,22 @@ server <- function(input, output) {
   })
 
   output$biomass_plot <- renderPlot(
-    ggplot(data = year_reactive(), aes(x = year, y = totBM)) +
-      geom_point(aes(color = mgmt, size = frmSz))
+    ggplot(data = year_reactive(), aes(x = year, y = tot_bm)) +
+      geom_point(aes(color = mgmt, size = frm_sz))
   )
 
   numfarms_reactive <- reactive({
 
     app_data %>%
-      filter(frmSz %in% input$num_farms)
+      filter(frm_sz %in% input$num_farms)
   })
 
   output$numfarms <- renderPlot(
     ggplot(data = numfarms_reactive(), aes(x = year, y = abund)) +
-      geom_jitter(aes(color = farmsize))
+      geom_jitter(aes(color = frmsz_class))+
+      facet_wrap(~mgmt) +
+      labs(color = "Farm Size", x = "Number of Years", y = "Abundance of Fish")+
+      theme_minimal()
   )
 
 }
